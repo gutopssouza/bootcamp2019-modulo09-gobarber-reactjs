14c50fd106ef3a0da9fc624cc199146ad6d1c6f6
Augusto <gutopssouza@gmail.com>
2019-09-10 19:39:34 -0300

    :art: Página de perfil :art: Implement style for profile page :rocket: Load profile data from state using react hooks on the form using Unform lib property initialData from RocketSeat

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+        <hr />
+        <Input name="oldPassword" type="password" placeholder="Sua senha" />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input
+          name="confirmaPassword"
+          type="password"
+          placeholder="Confirmação de senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
